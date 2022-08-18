a76f5839f38417e177af1af51d9c7c3b0ef4a7c2
Aaron Menadue <aaron.menadue@gmail.com>
2022-08-18 15:29:22 +0930

    cleaned client.c 'had global varibles for no particular reason'

--- client.c
+++ client.c
@@ -6,16 +6,13 @@
 /*   By: amenadue <amenadue@student.42adel.org.a    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/08 09:25:48 by amenadue          #+#    #+#             */
-/*   Updated: 2022/08/18 15:21:24 by amenadue         ###   ########.fr       */
+/*   Updated: 2022/08/18 15:28:29 by amenadue         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdlib.h>
 #include <signal.h>
-
-char	g_buf;
-char	g_i;
 
 void	send_string(int pid, char *str, size_t len)
 {
